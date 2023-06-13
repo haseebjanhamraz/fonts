8f81307e4fe18bed57bbbf766dc403a13a62e8d2
Haseeb Jan Hamraz <64940162+haseebjanhamraz@users.noreply.github.com>
2023-06-14 02:29:11 +0500

    Changed the code for good purpose

--- index.js
+++ index.js
@@ -1,60 +1,63 @@
-const { createCanvas, registerFont } = require('canvas');
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
+const express = require('express');
 
-function getAllFilenamesWithoutExtension(folderPath) {
-  const items = fs.readdirSync(folderPath);
-  const filenamesWithoutExtension = items.map(item => {
-    const itemPath = path.join(folderPath, item);
-    const parsedPath = path.parse(itemPath);
-    return parsedPath.name;
+const app = express();
+
+const fontsDirectory = path.join(__dirname, 'fonts');
+
+// Serve static files from the "fonts" directory
+app.use(express.static(fontsDirectory));
+
+// Read font files from the directory
+function getFontFiles() {
+  return fs.readdirSync(fontsDirectory);
+}
+
+// Display the headings with the respective font styles
+app.get('/', (req, res) => {
+  const fontFiles = getFontFiles();
+
+  let fontRules = ''; // Store all font rules in a single variable
+
+  fontFiles.forEach((filename) => {
+    const fontName = path.basename(filename, path.extname(filename));
+
+    // Generate the CSS rule for the font
+    const fontRule = `
+      @font-face {
+        font-family: '${fontName}';
+        src: url('/${filename}') format('truetype');
+      }
+    `;
+
+    fontRules += fontRule; // Append each font rule to the variable
   });
-  return filenamesWithoutExtension;
-}
 
-const folderPath = 'fonts';
-const filenames = getAllFilenamesWithoutExtension(folderPath);
-for (let i = 0; i < filenames.length; i++) {
-    const font = filenames[i];
-    console.log(font);
-}
+  let headings = '';
+  fontFiles.forEach((filename) => {
+    const fontName = path.basename(filename, path.extname(filename));
 
+    headings += `<h1 style="font-family: '${fontName}';">${fontName}</h1>`;
+  });
 
-const canvasWidth = 400;
-const canvasHeight = 200;
+  const html = `
+    <html>
+      <head>
+        <style>
+          ${fontRules} // Include all font rules in the style tag
+        </style>
+      </head>
+      <body>
+        ${headings}
+      </body>
+    </html>
+  `;
 
+  res.send(html);
+});
 
-
-// Register the font file
-const fontPath = path.join(__dirname, 'fonts', 'SFCompact.ttf');
-registerFont(fontPath, { family: 'SF Arabic' });
-
-// Create a canvas and set its dimensions
-const canvas = createCanvas(canvasWidth, canvasHeight);
-const context = canvas.getContext('2d');
-
-// Set the font style
-const fontSize = 36;
-context.font = `${fontSize}px 'SF Arabic'`;
-
-// Set the text color and alignment
-context.fillStyle = 'black';
-context.textAlign = 'center';
-context.textBaseline = 'middle';
-
-// Set the text content
-const text = 'یہ اردو فونٹس ہیں';
-
-// Calculate the text position
-const textX = canvasWidth / 2;
-const textY = canvasHeight / 2;
-
-// Draw the text on the canvas
-context.fillText(text, textX, textY);
-
-// Export the canvas as a PNG image
-const imagePath = path.join(__dirname, 'output.png');
-const out = fs.createWriteStream(imagePath);
-const stream = canvas.createPNGStream();
-stream.pipe(out);
-out.on('finish', () => console.log('Image saved:', imagePath));
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
